ae08c7f140542dfe233b01332181b15469962c77
VicRaii <vimarupon@hotmail.com>
2025-02-21 13:31:56 +0100

    add 2 more useStates

--- src/components/HouseMembers/HouseMembers.jsx
+++ src/components/HouseMembers/HouseMembers.jsx
@@ -1,8 +1,11 @@
 import { useEffect, useState } from 'react'
+import { FaSyncAlt } from 'react-icons/fa'
 import './HouseMembers.css'
 
 const HouseMembers = ({ houseName }) => {
   const [characters, setCharacters] = useState([])
+  const [flippedCards, setFlippedCards] = useState({})
+  const [showHint, setShowHint] = useState(true)
 
   useEffect(() => {
     fetch('https://hp-api.herokuapp.com/api/characters')
@@ -10,7 +13,16 @@ const HouseMembers = ({ houseName }) => {
       .then((res) => {
         setCharacters(res.filter((character) => character.house === houseName))
       })
+
+    setTimeout(() => setShowHint(false), 4000)
   }, [houseName])
+
+  const toggleFlip = (id) => {
+    setFlippedCards((prev) => ({
+      ...prev,
+      [id]: !prev[id]
+    }))
+  }
 
   return (
     <section className={`house ${houseName.toLowerCase()} flex_className`}>
@@ -21,20 +33,52 @@ const HouseMembers = ({ houseName }) => {
         {characters
           .filter((character) => character.image || !character.alive)
           .map((character) => (
-            <ul
+            <div
               key={character.id}
-              className={`house_card ${houseName.toLowerCase()}_card flex_className`}
+              className={`house_card ${houseName.toLowerCase()}_card ${
+                showHint ? 'shake' : ''
+              }`}
+              onClick={() => toggleFlip(character.id)}
             >
-              <li>
-                <h1>{character.name}</h1>
-              </li>
-              <li className='img_container'>
-                <img
-                  src={character.alive ? character.image : '/assets/RIP.jpg'}
-                  alt={character.name}
-                />
-              </li>
-            </ul>
+              <div
+                className={`card_inner ${
+                  flippedCards[character.id] ? 'flipped' : ''
+                }`}
+              >
+                <div className='card_front'>
+                  <ul>
+                    <li>
+                      <h1>{character.name}</h1>
+                    </li>
+                    <li className='img_container'>
+                      <img
+                        src={
+                          character.alive ? character.image : '/assets/RIP.jpg'
+                        }
+                        alt={character.name}
+                      />
+                    </li>
+                  </ul>
+                  <div className='flip_icon'>
+                    <FaSyncAlt />
+                  </div>
+                  {showHint && (
+                    <div className='hint'>¡Haz clic para ver más!</div>
+                  )}
+                </div>
+                <div className='card_back'>
+                  <h2>{character.alternate_names[0]}</h2>
+                  <h4>Date of Birth: {character.dateOfBirth}</h4>
+                  <h4>Ancestry: {character.ancestry}</h4>
+                  {/* <h4>
+                    Wand: Wood: {character.wand.wood}, Core:{' '}
+                    {character.wand.core}, Length: {character.wand.length}
+                  </h4> */}
+
+                  <p>Actor: {character.actor}</p>
+                </div>
+              </div>
+            </div>
           ))}
       </div>
     </section>
